0d2a8ba20d4c0e06e39cb5ef938d7fcfcb6ae0c5
Gouri Patankar <140386172+GouriPatankar@users.noreply.github.com>
2024-06-22 12:32:05 +0530

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,45 @@
+const enddate = "23 June  2024 12:50 PM"
+
+document.getElementById("end-Date").innerText=enddate;
+const inputs = document.querySelectorAll("input")
+
+// const clock = () =>{
+
+// }
+
+function clock() {
+    const end = new Date(enddate);
+    const now = new Date();
+    // console.log(end);
+    // console.log(now);
+    const diff = (end - now)/1000; // milisecond convert into seconds
+    // console.log(diff)
+    // console.log(Math.floor(diff/3600/24)) // showing the difference sec or hr = divide
+    inputs[0].value = Math.floor(diff/3600/24) //==days
+    inputs[1].value = Math.floor((diff/3600)%24) //=hr
+    inputs[2].value = Math.floor((diff/60)%60) //=min
+    inputs[3].value = Math.floor(diff%60) //=sec 
+     
+    // convert into days
+
+}   
+
+//initial call
+clock()
+
+
+/*
+1 day = 24 hr
+1 hour = 60 mint
+60 min = 3600 sec 
+*/
+setInterval(() => {
+    clock();
+}, 1000);
+
+// setInterval {
+//     () => {
+//         clock()
+//     },
+//     1000
+// } 
